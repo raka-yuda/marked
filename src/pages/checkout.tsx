22aeed335ebf007e74af66a5dcd4966b5c26352f
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-10-19 19:09:58 +0700

    add data cart to checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,12 +1,64 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { removeAllCartProduct, UserState } from "../redux/slices/user-slice";
+import { RootState } from "../redux/store";
+import { ProductsState } from "../redux/slices/product-slice";
+import { useRouter } from "next/router";
+import { CartProduct } from "../types/cart-product";
+import { useEffect, useState } from "react";
 
 const Checkout: NextPage = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const { cart, isLoggedIn }: UserState = useSelector(
+    (state: RootState) => state.user
+  );
+
+  const { products, status }: ProductsState = useSelector(
+    (state: RootState) => state.products
+  );
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      cart.forEach((product) => {
+        setTotalPrice((prevState) => {
+          const rawPrice = product.quantity * parseFloat(product.price);
+          const num = Number(rawPrice); // The Number() only visualizes the type and is not needed
+          const roundedString = num.toFixed(2);
+          const roundedPrice = Number(roundedString);
+          return prevState + roundedPrice;
+        });
+      });
+    }
+  }, [cart]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/products");
+    }
+  }, [isLoggedIn]);
+
+  const payCartProduct = (cart: CartProduct[]) => {
+    console.log("pay");
+    if (cart.length > 0) {
+      dispatch(removeAllCartProduct());
+      // console.log(cart);
+      router.push("/transaction");
+    }
+  };
+
+  const backToPreviousPage = () => {
+    router.back();
+  };
   return (
     <div>
-      <div className={`flex flex-row md:h-screen`}>
-        <div className="flex w-1/2 flex-col bg-gray-600">
+      <div className={`flex flex-col-reverse md:flex-row md:h-screen`}>
+        <div className="flex md:w-1/2 w-full p-12 flex-col bg-gray-600">
           <div className="flex flex-grow justify-center items-center">
             <div
               className={`flex justify-center items-center h-80 w-72 p-4 bg-gray-500`}
@@ -17,9 +69,9 @@ const Checkout: NextPage = () => {
         </div>
         <div className="flex overflow-y-auto w-full md:w-1/2 flex-col md:px-40 px-4 py-12">
           <div className="flex justify-end items-center py-4">
-            <Link href={`/homepage`}>
+            <button onClick={backToPreviousPage}>
               <a className={`text-base `}>Cancel</a>
-            </Link>
+            </button>
           </div>
           <div className="flex flex-col flex-grow justify-center">
             <div className="flex flex-row items-center">
@@ -27,7 +79,9 @@ const Checkout: NextPage = () => {
               <p className={`text-xl font-bold px-8`}>Marked</p>
             </div>
             <div className="flex flex-row items-center pb-12">
-              <p className={`text-5xl font-bold pt-4 pb-8`}>Checkout</p>
+              <p className={`md:text-5xl text-4xl font-bold pt-4 pb-8`}>
+                Checkout
+              </p>
             </div>
             <label htmlFor="username" className="text-sm text-gray-400">
               Full Name
@@ -36,7 +90,7 @@ const Checkout: NextPage = () => {
               <input
                 type="text"
                 placeholder="Enter username"
-                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 ..."
+                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 rounded-none"
               />
             </div>
             <label htmlFor="username" className="text-sm text-gray-400">
@@ -46,7 +100,7 @@ const Checkout: NextPage = () => {
               <input
                 type="text"
                 placeholder="Enter password"
-                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 ..."
+                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 rounded-none"
               />
             </div>
             <label htmlFor="username" className="text-sm text-gray-400">
@@ -56,20 +110,23 @@ const Checkout: NextPage = () => {
               <input
                 type="text"
                 placeholder="Enter password"
-                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 ..."
+                className="mt-2 p-4 border-2 border-black w-full focus:outline-none focus:ring focus:border-blue-300 rounded-none"
               />
             </div>
             <div className="flex flex-row justify-between py-3">
-              <p className={`text-xl font-bold `}>Marked</p>
-              <p className={`text-xl font-bold `}>$40</p>
+              <p className={`text-xl font-bold `}>Total Price</p>
+              <p className={`text-xl font-bold `}>${totalPrice}</p>
             </div>
 
             <div className="flex w-full pt-3 pb-8">
-              <div className="mt-4 py-1 bg-gray-500 w-full">
+              <button
+                onClick={() => payCartProduct(cart)}
+                className="mt-4 bg-gray-500 w-full"
+              >
                 <p className={`text-white text-base p-4 text-center`}>
                   Checkout
                 </p>
-              </div>
+              </button>
             </div>
           </div>
         </div>
