c2e128b5e08568a321cb6ea4c495cc23c35698a4
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-10-19 19:10:56 +0700

    update with dynamic data product

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -3,8 +3,30 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { MainNavbar } from "../components";
+import { useDispatch, useSelector } from "react-redux";
+import { Product } from "../types/product";
+import { addProductToUserCart, UserState } from "../redux/slices/user-slice";
+import { RootState } from "../redux/store";
+import CardProduct from "../components/card/card-product";
+import { fetchProducts, ProductsState } from "../redux/slices/product-slice";
+import { useEffect } from "react";
 
 const Products: NextPage = () => {
+  const { cart, isLoggedIn }: UserState = useSelector(
+    (state: RootState) => state.user
+  );
+
+  const { products, status }: ProductsState = useSelector(
+    (state: RootState) => state.products
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+    console.log(products);
+  }, [dispatch, fetchProducts]);
+
   return (
     <div>
       <MainNavbar />
@@ -16,96 +38,27 @@ const Products: NextPage = () => {
           <p className={`text-xl font-bold px-8`}>Explore all products</p>
         </div>
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
+          {status === "loading" && (
+            <div className="col-span-4 w-100 justify-center py-52">
+              <p className="font-bold text-center">
+                Wait for Fetching Data Products . . .{" "}
               </p>
-              <p className={`text-xl font-bold`}>$48</p>
             </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
-          <li className={`p-4 bg-gray-400`}>
-            <div className={`flex justify-center items-center h-60`}>M</div>
-            <div className="flex flex-row justify-between items-center">
-              <p className={`text-xl font-bold`}>
-                Brand New Black and White T-shirt
-              </p>
-              <p className={`text-xl font-bold`}>$48</p>
-            </div>
-          </li>
+          )}
+
+          {status === "succeeded" &&
+            products &&
+            products.map((product) => {
+              return (
+                <CardProduct
+                  key={`card-product-${product.id}`}
+                  title={`${product.title}`}
+                  price={`${product.price}`}
+                  bgImage={`${product.image}`}
+                  href={`/product/${product.id}`}
+                />
+              );
+            })}
         </ul>
       </div>
     </div>
