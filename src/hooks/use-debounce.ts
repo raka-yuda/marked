a20879ca5f3cf75a950083430e5a1f8a6948a159
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-10-19 19:03:22 +0700

    add use debounce hooks for delay hitting api

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+//Custom hooks for set delay when hitting async function
+export default function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+}
