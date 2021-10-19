f9a846b5e1a61e5a63e0d6b6a0cbae6d94e1eb9f
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-10-19 19:00:46 +0700

    add new utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -11,5 +13,18 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, addComponents, e, prefix, config }) {
+      // Add your custom styles here
+    }),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.border-spacing-0': {
+          [`border-spacing`]: '0',
+        }
+      }
+
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    })
+  ]
 }
