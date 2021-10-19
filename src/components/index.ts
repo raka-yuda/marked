1234e43b28cafff4c1e8e13ac3d30d8fda26c6ba
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-10-19 19:01:18 +0700

    register components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 import MainNavbar from "./navbar/main-navbar";
+import MainAlert from "./alert/main-alert";
+import RowProduct from "./product/row-product";
 
-export { MainNavbar };
+export { MainNavbar, MainAlert, RowProduct };
